2363ca713a9d7c342183ec925d3815e111746265
Pooja sri <143705699+Poojasri06@users.noreply.github.com>
2025-07-18 14:49:23 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# D:/Aura_derm/app/main.py
+
+import torch
+import torch.nn as nn
+from torchvision import models
+
+class SkinClassifier(nn.Module):
+    def __init__(self, num_classes=4):
+        super(SkinClassifier, self).__init__()
+        self.model = models.resnet18(pretrained=False)
+        self.model.fc = nn.Linear(self.model.fc.in_features, num_classes)
+
+    def forward(self, x):
+        return self.model(x)
+        st.markdown("### Your Skin Condition Prediction")       
